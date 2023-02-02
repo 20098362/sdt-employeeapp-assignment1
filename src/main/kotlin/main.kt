dba02790ebc72ef9e7bced471063229df88b387d
Troy <20098362@mail.wit.ie>
2023-02-02 16:01:26 +0000

    Update main.kt. Initialise payslip values

---
+++ src/main/kotlin/main.kt
@@ -0,0 +1,22 @@
+package ie.setu.employeeapp
+
+fun main(args: Array<String>){
+
+    val firstName = "Joe"
+
+    val lastName = "Soap"
+
+    val gender = "m"
+
+    val employeeId = 6143
+
+    val grossSalery = 67_543.21
+
+    val monthlyPaye = 38.5
+
+    val monthlyPrsi = 5.2
+
+    val annualBonus = 1_450.50
+
+    val monthlyCycleDeduction = 54.33
+}
