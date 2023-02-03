f97713f437a392bea7d363e220c6c7ef525c7b4b
Troy <20098362@mail.wit.ie>
2023-02-03 09:55:44 +0000

    Make payslip template. Enter figures into template

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,22 +1,66 @@
 package ie.setu.employeeapp
 
+import java.util.*
+import kotlin.math.roundToInt
+
+
+val firstName = "Joe"
+
+val lastName = "Soap"
+
+val gender = "m"
+
+val employeeId = 6143
+
+val grossSalary = 67_543.21
+
+val payePercentage = 38.5
+
+val prsiPercentage = 5.2
+
+val annualBonus = 1_450.50
+
+val monthlyCycleDeduction = 54.33
+
 fun main(args: Array<String>){
 
-    val firstName = "Joe"
+    printPayslip()
+}
 
-    val lastName = "Soap"
+fun printPayslip(){
 
-    val gender = "m"
+    val monthlySalary = (grossSalary/12)
 
-    val employeeId = 6143
+    val monthlyPrsi = monthlySalary * (prsiPercentage/100)
 
-    val grossSalery = 67_543.21
+    val monthlyPaye = monthlySalary * (payePercentage / 100)
 
-    val monthlyPaye = 38.5
+    val grossPay = (monthlySalary + (annualBonus/12))
 
-    val monthlyPrsi = 5.2
+    val totalDeductions = (monthlyPrsi + monthlyPrsi + monthlyCycleDeduction)
 
-    val annualBonus = 1_450.50
 
-    val monthlyCycleDeduction = 54.33
+    val printOut = """
+        >|_______________________________________________________________________|
+        >|                             Monthly Payslip                           |
+        >|_______________________________________________________________________|
+        >|                                                                       |
+        >|     Employee Name: ${firstName.uppercase()} ${lastName.uppercase()}               Gender: ${gender.uppercase()}                   |
+        >|                                                                       |
+        >|_______________________________________________________________________|
+        >|                                                                       |
+        >|     PAYMENT DETAILS                     DEDUCTION DETAILS             |
+        >|_______________________________________________________________________|
+        >|                                                                       |
+        >|     Salary: $monthlySalary           PRSI: $monthlyPrsi                       |
+        >|     Bonus: ${annualBonus/12}                      PAYE: $monthlyPaye                        |
+        >|                                         Cycle To Work: $monthlyCycleDeduction          |
+        >|_______________________________________________________________________|
+        >|     Gross: $grossPay            Total Deductions: $totalDeductions    |
+        >|_______________________________________________________________________|
+        >|                           Net Pay: ${(grossPay - totalDeductions)}                            |
+        >|_______________________________________________________________________|
+        """.trimMargin(">")
+
+    println(printOut)
 }
