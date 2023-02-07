d199e9b188b269f3f74a6bda2821ba0569315a0f
Troy <20098362@mail.wit.ie>
2023-02-07 10:07:10 +0000

    Refactor payslip raw string to be dynamic

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -4,25 +4,16 @@ import kotlin.math.round
 
 //Values used in the payslip
 val firstName = "Joe"
-
 val lastName = "Soap"
-
 val gender = "m"
-
 val employeeId = 6143
-
 val grossSalary = 67_543.21
-
 val payePercentage = 38.5
-
 val prsiPercentage = 5.2
-
 val annualBonus = 1_450.50
-
 val monthlyCycleDeduction = 54.33
 
 fun main(args: Array<String>){
-
     printPayslip()
 }
 
@@ -42,27 +33,30 @@ fun printPayslip(){
 
     val totalDeductions = roundTwoDecimals((monthlyPrsi + monthlyPrsi + monthlyCycleDeduction))
 
-
     val printOut = """
-        >|_______________________________________________________________________|
-        >|                             Monthly Payslip                           |
-        >|_______________________________________________________________________|
-        >|                                                                       |
-        >|     Employee Name: ${firstName.uppercase()} ${lastName.uppercase()}             Gender: ${gender.uppercase()}                   |
-        >|                                                                       |
-        >|_______________________________________________________________________|
-        >|                                                                       |
-        >|     PAYMENT DETAILS                     DEDUCTION DETAILS             |
-        >|_______________________________________________________________________|
-        >|                                                                       |
-        >|     Salary: $monthlySalary                      PRSI: $monthlyPrsi                  |
-        >|     Bonus: ${roundTwoDecimals(annualBonus / 12)}                       PAYE: $monthlyPaye                 |
-        >|                                         Cycle To Work: $monthlyCycleDeduction          |
-        >|_______________________________________________________________________|
-        >|     Gross: $grossPay                      Total Deductions: $totalDeductions      |
-        >|_______________________________________________________________________|
-        >|                           Net Pay: ${roundTwoDecimals(grossPay - totalDeductions)}                            |
-        >|_______________________________________________________________________|
+        >_______________________________________________________________________
+        >                             MONTHLY PAYSLIP
+        >_______________________________________________________________________
+        >                                                                       
+        >     EMPLOYEE NAME: ${firstName.uppercase()} ${lastName.uppercase()} (${gender.uppercase()}), ID: $employeeId
+        >_______________________________________________________________________
+        > 
+        >     PAYMENT DETAILS (Total Payment: $grossPay)
+        >_______________________________________________________________________
+        >        Salary: $monthlySalary
+        >        Bonus: ${roundTwoDecimals(annualBonus) / 12}
+        >_______________________________________________________________________
+        >                                                                       
+        >     DEDUCTION DETAILS (Total Deductions: $totalDeductions)
+        >_______________________________________________________________________
+        >                                         
+        >        PRSI: $monthlyPrsi
+        >        PAYE: $monthlyPaye
+        >        Cycle To Work: $monthlyCycleDeduction
+        >_______________________________________________________________________
+        >
+        >     NET PAY: ${roundTwoDecimals(grossPay - totalDeductions)}                            
+        >_______________________________________________________________________
         """.trimMargin(">")
 
     println(printOut)
